0ad39b634a927eeba3a906a1963c83c176ba4e89
Owen <owinship@umich.edu>
2020-09-17 02:04:01 -0400

    letsgo

--- lab4/partD1.c
+++ lab4/partD1.c
@@ -5,13 +5,20 @@ int main(){
 	char valuepath[] = "/sys/class/gpio/gpio23/value";
 	FILE *direction = fopen(dirpath,'w');
 	FILE *value = fopen(valuepath,'w');
-
+	
+	printf("lmao");
 	fprintf(direction,"out");
+	printf("rofl");
 	fclose(direction);
+	printf("lol");
 	for(;;){
 		fprintf(value,"%d",1);
+		printf("holy shit");
 		rewind(value);
+		printf("fug");
 		fprintf(value,"%d",0);
+		printf("waddup");
 		rewind(value);
+		printf("notlikethis");
 	}
 }
