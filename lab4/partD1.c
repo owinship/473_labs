9bcc248d25705d3f8fc48ec7dc9b0416ec5dcecc
Owen <owinship@umich.edu>
2020-09-17 02:00:25 -0400

    letsgo

--- lab4/partD1.c
+++ lab4/partD1.c
@@ -3,8 +3,8 @@
 int main(){
 	char dirpath[] = "/sys/class/gpio/gpio23/direction";
 	char valuepath[] = "/sys/class/gpio/gpio23/value";
-	FILE direction = fopen(dirpath,'w');
-	FILE value = fopen(valuepath,'w');
+	FILE *direction = fopen(dirpath,'w');
+	FILE *value = fopen(valuepath,'w');
 
 	fprintf(direction,"out");
 	fclose(direction);
