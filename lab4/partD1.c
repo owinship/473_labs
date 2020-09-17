113dbc64bbf8c2798b6be4eeddcd3a4261869d6b
Owen <owinship@umich.edu>
2020-09-17 02:15:21 -0400

    letsgo

--- lab4/partD1.c
+++ lab4/partD1.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	printf("in the beginning \n");
 	char dirpath[] = "/sys/class/gpio/gpio23/direction";
 	char valuepath[] = "/sys/class/gpio/gpio23/value";
-	printf("life was simple\n");
 	FILE *direction;
 	FILE *value;
 	direction = fopen(dirpath,"w");
@@ -14,19 +12,13 @@ int main(){
 		return 1;
 	}
 	
-	printf("lmao\n");
 	fprintf(direction,"out\n");
-	printf("rofl\n");
 	fclose(direction);
-	printf("lol\n");
 	for(;;){
 		fprintf(value,"%d",1);
-		printf("holy shit\n");
 		rewind(value);
 		printf("fug\n");
-		fprintf(value,"%d",0);
 		printf("waddup\n");
 		rewind(value);
-		printf("notlikethis\n");
 	}
 }
